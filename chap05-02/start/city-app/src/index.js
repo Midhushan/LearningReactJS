63070b179fa6ca8d5a681ef51715a2f7836618d8
Midhushan <39880516+Midhushan@users.noreply.github.com>
2019-11-30 10:29:31 -0800

    done

--- chap05-02/start/city-app/src/index.js
+++ chap05-02/start/city-app/src/index.js
@@ -4,8 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const DistrictList = [
+    {id: 1, name: "Kilinochchi", head:"KN"},
+    {id: 2, name: "Jaffna", head:"JF"},
+    {id: 3, name: "Mannar", head:"MN"},
+    {id: 4, name: "Vuaniya", head:"VN"},
+    ];
 
+ReactDOM.render(
+    <App districts={DistrictList}/>,
+    
+    document.getElementById("root")
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
